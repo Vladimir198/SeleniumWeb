0b9769aa3c0921b98336fd0e4e81c7c21b3d3df9
Vladimir <vladimir.zhevagin@yandex.ru>
2021-11-01 21:29:21 +0300

    изменены селекторы

--- src/test/java/ru/netology/web/FormSubmissionTest.java
+++ src/test/java/ru/netology/web/FormSubmissionTest.java
@@ -53,7 +53,7 @@ public class FormSubmissionTest {
         driver.findElement(By.name("phone")).sendKeys("+79270000000");
         driver.findElement(By.className("checkbox")).click();
         driver.findElement(By.className("button")).click();
-        String text = driver.findElement(By.tagName("p")).getText();
+        String text = driver.findElement(By.cssSelector("[data-test-id=order-success]")).getText();
         assertEquals("Ваша заявка успешно отправлена! " +
                 "Наш менеджер свяжется с вами в ближайшее время.", text.trim());
     }
@@ -65,9 +65,20 @@ public class FormSubmissionTest {
         driver.findElement(By.name("phone")).sendKeys("+79270000000");
         driver.findElement(By.className("checkbox")).click();
         driver.findElement(By.className("button")).click();
-        String text = driver.findElement(By.className("input__sub")).getText();
+        String text = driver.findElement(By.cssSelector("[data-test-id=name] .input__sub")).getText();
         assertEquals("Имя и Фамилия указаные неверно. Допустимы только " +
                 "русские буквы, пробелы и дефисы.", text.trim());
+    }
+
+    @Test
+    void shouldNegativePhone() {
+        driver.get("http://localhost:9999");
+        driver.findElement(By.className("input__control")).sendKeys("Василий");
+        driver.findElement(By.name("phone")).sendKeys("259270000000");
+        driver.findElement(By.className("checkbox")).click();
+        driver.findElement(By.className("button")).click();
+        String text = driver.findElement(By.cssSelector("[data-test-id=phone] .input__sub")).getText();
+        assertEquals("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678.", text.trim());
     }
 
 }
